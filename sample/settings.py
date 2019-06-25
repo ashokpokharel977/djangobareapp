26a97269644e864cc9b1295b3ae1585c610b0353
ashokpokharel977 <ashokpokharel977@gmail.com>
2019-06-25 20:25:20 +0545

    synced all files

--- sample/settings.py
+++ sample/settings.py
@@ -16,8 +16,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'irsmkfqlft#v3u%1#5i157k##1ke-iftcs8^(4*-a-zhba%p8s'
