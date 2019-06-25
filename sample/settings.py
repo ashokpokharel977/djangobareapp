b80d72cffc01240cab7f4101ae79a192848f657f
ashokpokharel977 <ashokpokharel977@gmail.com>
2019-06-25 17:22:33 +0545

    buildspec

--- sample/settings.py
+++ sample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'irsmkfqlft#v3u%1#5i157k##1ke-iftcs8^(4*-a-zhba%p8s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
